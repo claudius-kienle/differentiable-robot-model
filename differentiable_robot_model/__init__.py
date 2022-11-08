c78316fb7b07ba2e926f94492b48e2c1dbbb246f
Claudius <claudius.kienle@artiminds.com>
2022-11-08 08:24:09 +0100

    fixed imports

--- differentiable_robot_model/__init__.py
+++ differentiable_robot_model/__init__.py
@@ -3,10 +3,3 @@ Differentiable Robot Model
 ====================================
 Differentiable and learnable robot model.
 """
-
-from .robot_model import (
-    DifferentiableRobotModel,
-    DifferentiableKUKAiiwa,
-    DifferentiableFrankaPanda,
-    DifferentiableTwoLinkRobot,
-)
