50f39838dc32189d41096e155821d57f354db5ea
Claudius <claudius.kienle@artiminds.com>
2022-11-09 08:16:56 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,5 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='differentiable-robot-model',
-    version='1.0',
-    description='Differentiable Robot Model',
-    author='ArtiMinds',
-    author_email='claudius.kienle@artiminds.com',
-    url='https://artiminds.com',
-    packages=[
-        'differentiable_robot_model'
-    ],
-    install_requires=[
-        'numpy',
-        'torch',
-        'pyquaternion',
-        'urdf_parser_py',
-        'tqdm',
-        'matplotlib',
-    ],
+    packages=[ 'differentiable_robot_model' ],
 )
